9054e45c6c0d8898f4a42af6e96047bd8817f035
Gogafem <34383352+Gogafem@users.noreply.github.com>
2017-12-26 23:21:29 +0100

    Potato
    potato

---
+++ PotatoApp/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using PotatoApp.Models;
+
+namespace PotatoApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("ApplicationDbContext")
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
